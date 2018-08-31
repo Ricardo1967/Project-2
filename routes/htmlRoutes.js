61bd33bb2084562006b292b47aaeeb2fdddce330
Ricardo1967 <36551151+Ricardo1967@users.noreply.github.com>
2018-08-30 20:05:47 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,16 +2,10 @@ module.exports = function(app) {
 
   // Load index page
   app.get("/", function(req, res) {
-<<<<<<< HEAD
-    db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
       });
-    });
-=======
-    res.render("index", {
->>>>>>> 958404735ddcef1e0eb6903d6a274d5d59665857
   });
-});
+ 
 
   // Load login page
   app.get("/login", function(req, res) {
