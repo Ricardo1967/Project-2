35fb4897fd1805c55e65720124b70336bac174cf
Furn <FernandaRojas@ip-192-168-1-65.ec2.internal>
2018-09-02 13:40:55 -0400

    added change

--- public/js/index.js
+++ public/js/index.js
@@ -1,135 +1,42 @@
-$(document).ready(fucn)
+$(document).ready(function () {
+  console.log("working")
+  function postResult() {
+    $.ajax({
+      type: "GET",
+      url: "/api/results",
+      contentType: "application/json",
+      success: function (data) {
 
-function postResult() { 
-  $.ajax({
-    type:"POST", 
-    url:"/api/results",
-    data: JSON.stringify(data), 
-    success: function(data) { 
-      alert("Success")
-    }
-  });
-}//postItem() 
+        //data = data.body;
 
-$("#searchResult").on('click', function() { 
-  console.log("Hey");
-  postResult();
+        //var contentTitle = data.body.title;
+
+        //title = JSON.stringify(data.body.title);
+        alert("Success");
+        console.log("results out");
+        console.log(data);
+        $("#searchResult").text(data.title);
+        //$("#movieTitle").text(contentTitle);
+      }
+    });
+  };
+
+  $("#searchForMovie").on('click', function () {
+    event.preventDefault();
+    console.log('inside click');
+    var searchInput = $("#searchContent").val();
+    console.log(searchInput);
+    $.ajax({
+      type: "GET",
+      url: "/api/movie/"+searchInput,
+      contentType: "application/json",
+      success: function (data) {
+      }
+    });
+
+  })
+
+
+  // postResult();
+
 });
-
-
-//-------GIVEN STARTED TEMPLATE! ----// 
-  // Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
- 
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-    // return $.ajax({
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   type: "POST",
-    //   url: "api/examples",
-    //   data: JSON.stringify(example)
-    // });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// refreshExamples gets new examples from the db and repopulates the list
-
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// // handleFormSubmit is called whenever we submit a new example
-// // Save the new example to the db and refresh the list
-// var handleFormSubmit = function(event) {
-//   event.preventDefault();
-
-//   var example = {
-//     text: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
-//   }
-
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
-
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
-
-// // handleDeleteBtnClick is called when an example's delete button is clicked
-// // Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// // Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
-
-// // Login Page JS
-
-// //   $("#enrollLink").click(function() { 
-// //     $("#signUpForm").toggle(); 
-// //     $("#loginForm").hide(); 
-// //     return false;
-// //   });
-  
-// //   $("#loginLink").click(function() { 
-// //     $("#loginForm").toggle(); 
-// //     $("signUpForm").hide();
-// //     return false;
-// // });
