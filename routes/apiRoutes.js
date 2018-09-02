90b0d450a130693eac4ed391ab7b04e63f759a50
Furn <FernandaRojas@ip-192-168-1-65.ec2.internal>
2018-09-02 13:43:49 -0400

    added change

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,68 @@
 var request = require("request");
 //the movie database API key info - test using movie avengers search ALL (movie and tv)
-var options = { method: 'GET',
-      url: 'https://api.themoviedb.org/3/search/multi',
-      qs: 
-      { include_adult: 'false',
-        page: '1',
-        query: 'avengers',
-        language: 'en-US',
-        api_key: 'aa0f636f0795b94933b7d8c1b188b57e' },
-      body: '{}' };
-
-module.exports = function(app) {
-
-  app.get("/api/results", function(req,res) { 
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-    
-        console.log("getting data");
-        res.json(response);
-      });
-  })
+var options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/search/multi',
+  qs:
+    {
+      include_adult: 'false',
+      page: '1',
+      query: 'avengers',
+      language: 'en-US',
+      api_key: 'aa0f636f0795b94933b7d8c1b188b57e'
+    },
+  body: '{}'
 };
 
-// INITIAL given db Template
-   // Get all examples
-  // app.get("/api/examples", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.json(dbExamples);
-  //   });
-  // });
+module.exports = function (app) {
+  app.get("/api/movie/:title", function (req, res) {
+    console.log('in movie');
+    var movieTitle = req.params.title;
+    var options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/search/multi',
+      qs:
+        {
+          include_adult: 'false',
+          page: '1',
+          query: movieTitle,
+          language: 'en-US',
+          api_key: 'aa0f636f0795b94933b7d8c1b188b57e'
+        },
+      body: '{}'
+    };
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
 
-  // // Create a new example
-  // app.post("/api/examples", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+      console.log("getting data");
+      response = JSON.parse(response.body);
+      var result = response.results[0];
 
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+      var responseObj = {
+        title: result.title
+      }
+      console.log(responseObj);
+
+      res.json(responseObj);
+    });
+    // reqCreate(movieTitle)
+  });
+
+  app.get("/api/results", function (req, res) {
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      console.log("getting data");
+      response = JSON.parse(response.body);
+      var result = response.results[0];
+
+      var responseObj = {
+        title: result.title
+      }
+      console.log(responseObj);
+
+      res.json(responseObj);
+    });
+  })
+
+};
