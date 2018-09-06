ddbcdbc54129f4ef4b5dc0bc81be65b16945f5bd
Liam Sweeney <liamsweeney@liam-sweeneys-computer.local>
2018-09-06 11:09:21 -0400

    final styling

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,28 @@
-$(document).ready(function () {
+$("#searchResults").hide();
 
-  //featured movies //
+$(document).ready(function () { 
+
+  //featured movies section//
   $.ajax({ 
     type: "GET", 
-    url: "/api/featured", 
+    url: "/api/featured/movies", 
     contentType: "application/json", 
     success: function(data) { 
       console.log(data); 
 
       for (var i = 0; i < 6; i++) { 
 
+        //var contentID = data.results[i].id;
         var title = data.results[i].original_title;
         var rating = data.results[i].vote_average;
         var image = "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
 
-        var featuredContainer = $("#featuredContainer");
+        var featuredContainer = $("#featuredMovieContainer");
 
         var cardDeckDiv = $("<div class='card-deck'>");
         var resultDiv = $("<div class='card mx-auto' style='width:175px'>");
 
-        var resultImage = $("<img class='card-img-top'>"); 
+        var resultImage = $("<img class='image card-img-top'>");
           resultImage.attr("src", image);
 
         var resultBody = $("<div class='card-body'>");
@@ -29,17 +32,57 @@ $(document).ready(function () {
         var addButton = $("<button id='addButton' type='button' class='btn btn-secondary btn-lg btn-block'>Add to List</button>");
 
         cardDeckDiv.append(resultDiv);
-        resultDiv.append(resultBody, resultImage, resultBody, titleDiv, ratingDiv, addButton)
+        resultDiv.append(resultBody, resultImage, titleDiv, ratingDiv, addButton)
         featuredContainer.prepend(resultDiv);
       }
     }
   });
-
+  //END of featured movies section//
  
+
+  //featured TV section//
+  $.ajax({ 
+    type: "GET", 
+    url: "/api/featured/tv", 
+    contentType: "application/json", 
+    success: function(data) { 
+      console.log(data); 
+
+      for (var i = 0; i < 6; i++) { 
+
+        var contentID = data.results[i].id;
+        var title = data.results[i].original_title;
+        var rating = data.results[i].vote_average;
+        var image = "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
+
+        var featuredContainer = $("#featuredTVContainer");
+
+        var cardDeckDiv = $("<div class='card-deck'>");
+          
+        var resultDiv = $("<div class='card mx-auto' style='width:175px'>");
+          
+        var resultImage = $("<img class='image card-img-top'>");
+          resultImage.attr("src", image);
+
+        var resultBody = $("<div class='card-body contentID'>");
+         //resultBody.attr('id', contentID);
+        var titleDiv = $("<h3 class='title card-title text-center'</h3>").text(title);
+
+        var ratingDiv = $("<p class='rating card-text text-center'</p>").text(rating);
+        var addButton = $("<button id='addButton' type='button' class='btn btn-secondary btn-lg btn-block'>Add to List</button>");
+
+        cardDeckDiv.append(resultDiv);
+        resultDiv.append(resultBody, resultImage, titleDiv, ratingDiv, addButton)
+        featuredContainer.prepend(resultDiv);
+      }
+    }
+  });
+  //END of featured movies section//
 
   //addButton push to DB
   $(document).on("click", "#addButton", function(){
     console.log("Hello");
+   // var $userSelectedID = $(this).parent('.card').find('.contentID').attr();
     var $userSelectedTitle = $(this).parent('.card').find('.title').text();
     var $userSelectedRating = $(this).parent('.card').find('.rating').text();
     var $userSelectedImage = $(this).parent('.card').find('image').text();
@@ -47,18 +90,17 @@ $(document).ready(function () {
     
       event.preventDefault();
       var watchList = {
+       // id: $userSelectedID,
         title: $userSelectedTitle,
         rating: $userSelectedRating,
         image: $userSelectedImage
       };
-  
       $.post("/api/movie", watchList);
-      
-    
   });
 
   //search results from search bar on click
   $("#searchForMovie").on('click', function () {
+    $("#searchResults").show();
     $("#resultsContainer").empty();
     $("#featuredContainer").empty();
       event.preventDefault();
@@ -80,7 +122,7 @@ $(document).ready(function () {
             var divContainer = $("#resultsContainer");
 
 
-            var resultDiv = $("<div class='card' style='width:275px'>");
+            var resultDiv = $("<div class='card mx-auto' style='width:175px'>");
 
             var resultBody = $("<div class='card-body'>");
             var resultImage = $("<img class=' imagecard-img-top'>");
